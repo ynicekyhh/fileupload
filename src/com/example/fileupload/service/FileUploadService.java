f57365c0e1270fa4e18bfa082250bc219dcb9458
kickscar-github <kickscar@gmail.com>
2017-09-15 06:52:25 +0900

    init commit

---
+++ src/com/example/fileupload/service/FileUploadService.java
@@ -0,0 +1,13 @@
+package com.example.fileupload.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileUploadService {
+	
+	public String restore(MultipartFile multipartFile) {
+		return "";
+	}
+
+}
